dabdf23c0bc671049f04126812844de8c5525777
RahJeremy <106093292+RahJeremy@users.noreply.github.com>
2022-05-24 12:54:02 -0400

    Update mypart2.py

--- python/activities/pe0/mypart2.py
+++ python/activities/pe0/mypart2.py
@@ -1,3 +1,26 @@
+  1 *Practical Exercise 0-2*
+  2 
+  3 ---
+  4 
+  5 FizzBuzz is an interview question that is said to filter out 99.5% of programming job candid    ates.
+  6 
+  7 ---
+  8 
+  9 *Deliverable*
+ 10 
+ 11 Modify deliverable.py so that it takes a number from the user and prints it (the number) if     it isn't divisible by 3 or 5. For multiples of 3 print 'fizz' instead. For multiples of 5 pr    int 'buzz' instead. For multiples of 3 and 5 print 'fizzbuzz'.
+ 12 
+ 13 ---
+ 14 
+ 15 .deliverable.py
+ 16 ----
+ 17 include::deliverable.py[]
+ 18 ----
+ 19 
+ 20 ---
+ 21 
+ 22 *END*
+~                
   '''
   FAILED
   #!/usr/bin/env python3
